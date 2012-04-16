85e26cfde54497db54637c48840c1687ac4a5ebf
Niranjan Paranjape <niranjangp@gmail.com>
2012-04-16 12:28:46 +0530

    duplicate line

---
+++ duplicate-line.el
@@ -0,0 +1,67 @@
+;;; duplicate-line.el --- Duplicate a line without adding it to kill-ring
+
+;; Copyright (C) 2012 Niranjan Paranjape - Based on discussion http://x.c42.in/IqvMvD
+;; Authors: Niranjan Paranjape
+;; URL: http://github.com/c42/duplicate-line.el
+;; Created: 2012
+;; Version: 1.0
+;; Keywords: duplicate line
+;; Note: 
+
+;;; Commentary:
+;;
+;; This file is NOT part of GNU Emacs.
+;;
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; See <http://www.gnu.org/licenses/> for a copy of the GNU General
+;; Public License.
+
+;;; Code:
+
+(defun duplicate-line (arg)
+  "Duplicate current line, leaving point in lower line."
+  (interactive "*p")
+
+  ;; save the point for undo
+  (setq buffer-undo-list (cons (point) buffer-undo-list))
+
+  ;; local variables for start and end of line
+  (let ((bol (save-excursion (beginning-of-line) (point)))
+        eol)
+    (save-excursion
+
+      ;; don't use forward-line for this, because you would have
+      ;; to check whether you are at the end of the buffer
+      (end-of-line)
+      (setq eol (point))
+
+      ;; store the line and disable the recording of undo information
+      (let ((line (buffer-substring bol eol))
+            (buffer-undo-list t)
+            (count arg))
+        ;; insert the line arg times
+        (while (> count 0)
+          (newline)         ;; because there is no newline in 'line'
+          (insert line)
+          (setq count (1- count)))
+        )
+
+      ;; create the undo information
+      (setq buffer-undo-list (cons (cons eol (point)) buffer-undo-list)))
+    ) ; end-of-let
+
+  ;; put the point in the lowest line and return
+  (next-line arg))
+
+(provide 'duplicate-line)
+
+;;; duplicate-line.el ends here
